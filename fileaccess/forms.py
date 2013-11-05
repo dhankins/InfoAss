8e69f0cf77a8d54ce5ec13e40cc3f7e9825f1cd8
Breton Goers <bgoers@asu.edu>
2013-11-05 09:55:50 -0700

    HTML and CSS changes for better look

--- fileaccess/forms.py
+++ fileaccess/forms.py
@@ -2,23 +2,45 @@ from django import forms
 from fileaccess.models import RWAttribute
 
 class AccessLoginForm(forms.Form):
-	username = forms.CharField(max_length=50, widget=forms.TextInput())
-	password = forms.CharField(max_length=50, widget=forms.PasswordInput())
+	username = forms.CharField(
+		max_length=50, 
+		widget=forms.TextInput(), 
+		error_messages={'required' : 'You must enter a username!'}
+	)
+	password = forms.CharField(
+		max_length=50, 
+		widget=forms.PasswordInput(), 
+		error_messages={'required' : 'You must enter a password!'}
+	)
 
 class NewFileForm(forms.Form):
 	CHOICES = ((att.pk, att.identifier) for att in RWAttribute.objects.all())
 
-	fileName = forms.CharField(max_length=100)
-	fileUpload = forms.FileField()
-	attributes = forms.MultipleChoiceField(choices=CHOICES, 
-											widget=forms.CheckboxSelectMultiple(), 
-											required=False)
+	fileName = forms.CharField(
+		max_length=100, 
+		error_messages={'required' : 'You must enter a file name!'}
+	)
+	fileUpload = forms.FileField(
+		error_messages={'required' : 'You must choose a file!'}
+	)
+	attributes = forms.MultipleChoiceField(
+		choices=CHOICES,  
+		widget=forms.CheckboxSelectMultiple(), 
+		required=False
+	)
 
 class UpdateFileForm(forms.Form):
 	CHOICES = ((att.pk, att.identifier) for att in RWAttribute.objects.all())
 
-	fileName = forms.CharField(max_length=100)
-	fileUpload = forms.FileField(required=False)
-	attributes = forms.MultipleChoiceField(choices=CHOICES, 
-											widget=forms.CheckboxSelectMultiple(), 
-											required=False)
+	fileName = forms.CharField(
+		max_length=100, 
+		error_messages={'required' : 'You must have a file name!'}
+	)
+	fileUpload = forms.FileField(
+		required=False
+	)
+	attributes = forms.MultipleChoiceField(
+		choices=CHOICES, 
+		widget=forms.CheckboxSelectMultiple(), 
+		required=False
+	)
